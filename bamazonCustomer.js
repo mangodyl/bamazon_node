144e62af7894b2e0fd8e79b76f0521fbecfade2d
mango dyl <dylanbonds@hotmail.com>
2019-02-20 22:49:03 -0600

    buyItems prompt sstarted

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,6 +12,7 @@ var connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err;
     displayItems();
+    setTimeout(buyItems(), 8000);
 });
 
 const displayItems = () => {
@@ -29,3 +30,23 @@ const displayItems = () => {
         };
     });
 };
+
+const buyItems = () => {
+    inquirer
+        .prompt([
+            {
+                name: "askBuy",
+                type: "confirm",
+                message: "Have you decided what you'd like to buy?"
+            }, {
+                name: "enterID",
+                type: "input",
+                validate: function(value) {
+                    let pass = value.match("\d{5}");
+                    if(pass) return true;
+                    else return "Please enter a valid ID";
+                },
+                message: "Enter the ID of the product you wish to purchase"
+            }
+        ])
+}
